9060b2b9c9b698139cd784abf06de0723a17e1c2
ead <ead@academico.local>
2025-04-29 10:22:20 -0300

    aula1 testando

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô, Git!");
+        System.out.println("JEsstatistics!");
+               
+        // variável : e um espaço reserevado ne memoria do computador 
+        // Variável : serve para guardad valores
+        // Variável : ela tem um tipo (texto, número, Lógio...)
+        /*
+         * Ex-1: crie um algoritimo para o JEsstatistics que leia a opção desejada:
+         * "Qual o con junto de dados utilizado: 
+         * 1- populacional 
+         * 2- amostral"
+         * O valor recebido deve ser um valor inteiro.
+         */
+        
+        int opcao; //declarrei a variável em java.
+        System.out.println("/t # /t # /tJEsstatistics!");
+        System.out.println("Qual o conjunto de dados utilizado: 1- populacional 2- amostral");
+        //opcao = leia.nextInt();
+        //System.out.println(opcao);
     }
 }
