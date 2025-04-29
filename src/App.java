f5c7c04d14e26817a3e17fa44c9b04a58c4c94f8
ead <ead@academico.local>
2025-04-29 11:38:11 -0300

    finalizando aula logica 1 - ok

--- src/App.java
+++ src/App.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("JEsstatistics!");
@@ -13,10 +17,27 @@ public class App {
          * O valor recebido deve ser um valor inteiro.
          */
         
+        
+        /*Scanner leia = new Scanner(System.in);
         int opcao; //declarrei a variável em java.
-        System.out.println("/t # /t # /tJEsstatistics!");
+        System.out.println("/t # /t # /t JStattistics!");
         System.out.println("Qual o conjunto de dados utilizado: 1- populacional 2- amostral");
-        //opcao = leia.nextInt();
+        opcao = leia.nextInt();
+        System.out.println(opcao);
+        */
+        int opcao; 
+        JOptionPane.showMessageDialog(null, "O Jogo da Estatística: JStattistics");
+        opcao = Integer.parseInt(JOptionPane.showInputDialog( "Vamso começar o jogo? \n Escolha a estratégia \n 1- Populacional \n 2- Amostraal"));
+        JOptionPane.showMessageDialog(null, "você escolheu:"+opcao,"JStattistics", JOptionPane.DEFAULT_OPTION);
+        
+               
+
         //System.out.println(opcao);
+        String opcaoVariavel = JOptionPane.showInputDialog(null, "O Jogo da Estatística: JStattistics");
+        int opVariavel = Integer.parseInt(opcaoVariavel);
+        JOptionPane.showMessageDialog(null, "Que tipo de variável ele deseja cadastrar:\n 1-Quantitativa \n 2-Qualitativa");
+        
+
+        
     }
 }
