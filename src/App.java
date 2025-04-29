3d081fa6c758f62bf00f727eb7f5492d3182d0d5
ead <ead@academico.local>
2025-04-29 10:16:29 -0300

    teste

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô, Git!");
+        System.out.println("\t\tJEsstatistics!");// centralizar \t\t
+        System.out.print("-- Controle de Dados Empresarial");
+        System.out.print("2025");
+        //ArrayList list = new ArrayList();
+        // Aplicando lógica de Programação (Aristeles e Geoge Boolean)
+        //importa tudo *
+       
+      
+    //comando das aulas 
+    // centralizar \t\t
+    // cd .. volta a pasta anterior 
+    //cd nome da pasta abre a seguinte 
+    //git branch acesar abrir dentro da pasta 
+    //inicio
+    //cd projetogit
+    //git checkout logica-aula1
     }
 }
