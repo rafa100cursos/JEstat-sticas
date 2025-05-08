a8bb13af992987c0edb490da3e6f88850120eee6
ead <ead@academico.local>
2025-05-08 11:52:54 -0300

    Atividade Manifero

--- src/App.java
+++ src/App.java
@@ -1,5 +1,35 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô, Git!");
+        // Classe e um conjunto de objetos
+        //Nosso objetivo nesse código é entender a diferença entre método com o tipo void e método com tipo string
+        Mamifero m1 = new Mamifero();
+        m1.setNomeCientifico("Bos Taurus");
+        JOptionPane.showMessageDialog(null, "nome Cientifico:"+m1.getNomeCientifico());
+
+        /*
+         * diferença entre função e procedimento
+         * set - void (procedimento)
+         * get - string (função)
+         * java = Métodos
+         *      void
+         *      string, int, boolean
+         * 
+         * Desafio:
+         * Crie mais dois objetos:
+         * Para armazenar e mostrar os dados
+         * Canguru
+         * Peixe Boi
+         */
+
+         Mamifero m2 = new Mamifero();
+        m2.setNomeCientifico("Trichechus");
+        JOptionPane.showMessageDialog(null, "nome Cientifico:"+m2.getNomeCientifico());
+
+        Mamifero m3 = new Mamifero();
+        m3.setNomeCientifico("Macropus rufus");
+        JOptionPane.showMessageDialog(null, "nome Cientifico:"+m3.getNomeCientifico());
+
     }
 }
---
+++ src/Mamifero.java
@@ -0,0 +1,19 @@
+public class Mamifero {
+
+    private int id;
+    private String nomeCientifico;
+
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setNomeCientifico(String nomeCientifico){
+        this.nomeCientifico = nomeCientifico;
+    }
+
+    public String getNomeCientifico(){
+        return this.nomeCientifico;
+    }
+
+}
