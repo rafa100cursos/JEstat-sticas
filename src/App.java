a63812a29ed5871a1cee7329d44c2976b88ac4c6
ead <ead@academico.local>
2025-05-09 09:49:43 -0300

    Atividade2

--- src/App.java
+++ src/App.java
@@ -3,8 +3,18 @@ import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        JOptionPane.showMessageDialog(null,  "Alo jardim");
+        JOptionPane.showMessageDialog(null,  "Alô jardim");
+
+           
+           String nome = JOptionPane.showInputDialog("Digite o nome do cliente:");
+           
+           String endereco = JOptionPane.showInputDialog("Digite o endereço do cliente:");
+           
+           
+           String mensagem = "Cliente cadastrado:\nNome: " + nome + "\nEndereço: " + endereco;
+           JOptionPane.showMessageDialog(null, mensagem);
+       }
        
 
-    }
+    
 }
