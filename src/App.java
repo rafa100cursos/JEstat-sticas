7b093767f513b003f0eefbddefe8e22ffbb0c1ec
ead <ead@academico.local>
2025-05-09 09:04:33 -0300

    Atividade1-Ok

--- src/App.java
+++ src/App.java
@@ -2,6 +2,8 @@ import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+        JOptionPane.showMessageDialog(null,  "Alo jardim");
        
 
     }
